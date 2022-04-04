d9dbc1c891d32e6487bac04150e8e8faa72b6b7b
choihy0310 <8546088@naver.com>
2022-04-04 18:43:15 +0900

    유효성 검사기능 추가 및 부트스트랩5 마이그레이션

--- ToDoListApi/src/main/resources/static/js/list.js
+++ ToDoListApi/src/main/resources/static/js/list.js
@@ -9,13 +9,13 @@ for(let i = 0; i < CompleteList.length; i++)
     if(CompleteList[i].textContent == "false"){
         CompleteList[i].textContent= "아직 진행중입니다."
         let tr =  CompleteList[i].parentNode;
-        tr.className = "danger"
+        tr.className = "table-danger"
 
     }
     if(CompleteList[i].textContent == "true"){
         CompleteList[i].textContent = "완료되었습니다."
         console.log(CompleteList[i].parentNode);
         let tr =  CompleteList[i].parentNode;
-        tr.className = "success"
+        tr.className = "table-success"
     }
 }
--- ToDoListApi/src/main/resources/static/js/mainpost.js
+++ ToDoListApi/src/main/resources/static/js/mainpost.js
@@ -1,3 +1,93 @@
+
+let isTitleValid = false;
+let isWhatToDoValid=false;
+let isWhenToDoValid =false;
+let isHowLongToDoValid = false;
+
+document.querySelector("#title").addEventListener("input", function(){
+
+    let inputtitle=this.value;
+
+    if(inputtitle.length == 0){
+        isTitleValid = false;
+    }
+    else{
+        isTitleValid = true;
+    }
+
+    if(isTitleValid == true){
+        this.classList.remove("is-invalid");
+        this.classList.add("is-valid");
+    }else{
+        this.classList.remove("is-valid");
+        this.classList.add("is-invalid");
+    }
+});
+
+document.querySelector("#whatToDo").addEventListener("input", function(){
+
+    let inputwhattodo=this.value;
+    console.log(inputwhattodo);
+
+    if(inputwhattodo.length == 0){
+        isWhatToDoValid = false;
+    }
+    else{
+        isWhatToDoValid = true;
+    }
+    if(isWhatToDoValid == true){
+        this.classList.remove("is-invalid");
+        this.classList.add("is-valid");
+    }else{
+        this.classList.remove("is-valid");
+        this.classList.add("is-invalid");
+    }
+});
+
+document.querySelector("#whenToDo").addEventListener("input", function(){
+
+    let inputwhentodo=this.value;
+
+    if(inputwhentodo.length == 0){
+        isWhenToDoValid = false;
+    }
+    else{
+        isWhenToDoValid = true;
+    }
+
+    if(isWhenToDoValid == true){
+        this.classList.remove("is-invalid");
+        this.classList.add("is-valid");
+    }else{
+        this.classList.remove("is-valid");
+        this.classList.add("is-invalid");
+    }
+});
+
+document.querySelector("#howLongToDo").addEventListener("input", function(){
+
+    let inputhowlong=this.value;
+
+    if(inputhowlong.value == 0){
+        isHowLongToDoValid = false;
+    }
+    else{
+        isHowLongToDoValid = true;
+    }
+
+    if(isHowLongToDoValid == true){
+        this.classList.remove("is-invalid");
+        this.classList.add("is-valid");
+    }else{
+        this.classList.remove("is-valid");
+        this.classList.add("is-invalid");
+    }
+});
+
+
+
+
+
 var btn_save = document.querySelector("#button_save");
 
 btn_save.addEventListener('click', function (event) {
@@ -37,7 +127,23 @@ btn_save.addEventListener('click', function (event) {
             alert('글이 등록되었습니다.');
             window.location.href = '/';
         } else { //통신 실패
-            alert("등록에 실패했습니다.")
+            if(!isTitleValid){
+                _title.classList.remove("is-valid");
+                _title.classList.add("is-invalid");
+            }
+            if(!isWhatToDoValid){
+                _whatToDo.classList.remove("is-valid");
+                _whatToDo.classList.add("is-invalid");
+            }
+            if(!isWhenToDoValid){
+                _whenToDo.classList.remove("is-valid");
+                _whenToDo.classList.add("is-invalid");
+            }
+            if(!isHowLongToDoValid){
+                _howLongTodo.classList.remove("is-valid");
+                _howLongTodo.classList.add("is-invalid");
+            }
+            alert("누락된 요소가 있습니다.")
             console.log("통신 실패");
         }
     }
